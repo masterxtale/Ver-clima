75f6774c00d3dfa98951e7b7054fe9aa81fe93e6
immAURORA <idreew2015@gmail.com>
2026-03-31 05:20:34 -0300

    tratamento dos dados para exibir a previsão do tempo para os próximos 5 dias, incluindo temperatura mínima e máxima, umidade e descrição do clima. temmperatura arredondada para o inteiro mais próximo. dia complexo trocado para dia da semana.

--- js/script.js
+++ js/script.js
@@ -10,19 +10,37 @@ document.getElementById("consulta").addEventListener("click", function() {
 async function buscarClima(cidade) {
   const resposta = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=8cb7a928f252f47575cf6ffe3c355750&units=metric&lang=pt_br`);
   const dados = await resposta.json();
-
+  console.log(dados);
   previsao(dados);
 }
 
 function previsao(dados){
   for(let i = 0; i < 40; i += 8) {
-
+    
+    
     const dia = (i / 8) + 1;
+    const data = new Date(dados.list[i].dt_txt);
+    let diaSemana = data.toLocaleDateString("pt-BR", {
+    weekday: "short"
+    });
+    diaSemana = diaSemana.replace(".", "");
+    document.getElementById("dia" + dia).innerHTML = diaSemana;
 
-    document.getElementById("dia" + dia).innerHTML = dados.list[i].dt_txt;
-    document.getElementById("temp" + dia).innerHTML = dados.list[i].main.temp + "°C";
+    
+
     document.getElementById("umidade" + dia).innerHTML = dados.list[i].main.humidity + "%";
     document.getElementById("status" + dia).innerHTML = dados.list[i].weather[0].description;
 
+    // 🔥 calcular temperaturas do dia
+    let temps = [];
+
+for (let j = i; j < i + 8 && j < dados.list.length; j++) {
+  temps.push(dados.list[j].main.temp);
+}
+    const min = Math.min(...temps);
+    const max = Math.max(...temps);
+
+    document.getElementById("tempMin" + dia).innerHTML = Math.round(min) + "°C";
+    document.getElementById("tempMax" + dia).innerHTML = Math.round(max) + "°C";
   }
 }
