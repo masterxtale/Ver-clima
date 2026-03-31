3f2ee79c80ef4842e67107f99e3eb48450cd4abd
immAURORA <idreew2015@gmail.com>
2026-03-30 21:51:41 -0300

    codigo para mostrar o clima de uma cidade usando a API do OpenWeatherMap. O código foi atualizado para incluir a chave de API correta e para exibir o nome da cidade no título da página. O código foi atualizado para exibir as informações de temperatura, vento e umidade para os próximos 5 dias, usando os dados retornados pela API do OpenWeatherMap.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-
-                                     //função para pegar o valor da cidade após apertar o buttom
 document.getElementById("consulta").addEventListener("click", function() {
-const cidade = document.getElementById("cidade1").value;
-alert(cidade);
-resultado.innerHTML = "A cidade selecionada é: " + cidade;
-buscarClima(cidade); //chama a função para buscar o clima da cidade
+  const cidade = document.getElementById("cidade1").value;
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = "Cidade: " + cidade;
+
+  buscarClima(cidade);
 });
 
-async function buscarClima(cidade) { //função para usar a cidade com a API clima
-const resposta =await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=8cb7a928f252f47575cf6ffe3c355750&units=metric&lang=pt_br`)
-const dados = await resposta.json(); //pega os dados
+async function buscarClima(cidade) {
+  const resposta = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=8cb7a928f252f47575cf6ffe3c355750&units=metric&lang=pt_br`);
+  const dados = await resposta.json();
 
-const resultado = document.getElementById("resultado");
-console.log(dados);
-resultado.innerHTML = `A temperatura em ${cidade} é de ${dados.main.temp}°C.`;
+  previsao(dados);
+}
+
+function previsao(dados){
+  for(let i = 0; i < 40; i += 8) {
+
+    const dia = (i / 8) + 1;
+
+    document.getElementById("dia" + dia).innerHTML = dados.list[i].dt_txt;
+    document.getElementById("temp" + dia).innerHTML = dados.list[i].main.temp + "°C";
+    document.getElementById("umidade" + dia).innerHTML = dados.list[i].main.humidity + "%";
+    document.getElementById("status" + dia).innerHTML = dados.list[i].weather[0].description;
+
+  }
 }
