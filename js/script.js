3e86fb376bff394a0d276c06b775f03b62233fc2
immAURORA <idreew2015@gmail.com>
2026-03-31 21:41:55 -0300

    mudando o layout do grid, adicionando um container externo para centralizar o grid e deixar ele mais bonito. Também adicionei um display none para o grid externo, para que ele só apareça quando a consulta for feita. E adicionei um alert para quando a cidade não for encontrada. normalização do código, removendo espaços em branco e organizando o código para ficar mais legível. melhoria no javaScript, adicionando um console.log para verificar os dados recebidos da API e também para verificar se o grid está sendo exibido corretamente. E também adicionei um display none para o grid externo quando a cidade não for encontrada.

--- js/script.js
+++ js/script.js
@@ -11,9 +11,21 @@ async function buscarClima(cidade) {
   const resposta = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=8cb7a928f252f47575cf6ffe3c355750&units=metric&lang=pt_br`);
   const dados = await resposta.json();
   console.log(dados);
-  previsao(dados);
-  grid.style.display = "grid";
+ 
+  if (dados.cod === "404") {
+    alert("Cidade não encontrada. Por favor, tente novamente.");
+    grid.style.display = "none"
+    gridExterno.style.display = "none";
+  }
+  else{
+    console.log(dados);
+    previsao(dados);
+    grid.style.display = "grid";
+    gridExterno.style.display = "block";
+  }
 }
+  
+
 
 function previsao(dados){
   for(let i = 0; i < 40; i += 8) {
@@ -32,7 +44,7 @@ function previsao(dados){
     document.getElementById("umidade" + dia).innerHTML = dados.list[i].main.humidity + "%";
     document.getElementById("status" + dia).innerHTML = dados.list[i].weather[0].description;
 
-    // 🔥 calcular temperaturas do dia
+    //calcula a temperaturas do dia
     let temps = [];
 
 for (let j = i; j < i + 8 && j < dados.list.length; j++) {
@@ -44,4 +56,12 @@ for (let j = i; j < i + 8 && j < dados.list.length; j++) {
     document.getElementById("tempMin" + dia).innerHTML = Math.round(min) + "°C";
     document.getElementById("tempMax" + dia).innerHTML = Math.round(max) + "°C";
   }
-}
+  for (let i = 0; i < 5; i++) {
+    const dia = i + 1;
+    const icone = dados.list[i*8].weather[0].icon;
+
+    document.getElementById("icone" + dia).style.backgroundImage =  `url("https://openweathermap.org/img/wn/${icone}@2x.png")`;
+      
+  };
+
+};
